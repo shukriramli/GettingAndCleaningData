8fb52390394af0804782a82ce223be9b07f12cb6
shukriramli <mshukri.ramli9@gmail.com>
2015-07-08 14:55:01 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,57 +1,49 @@
-# This R script does the following:
+## step 1
+# read all the data
+test.labels <- read.table("test/y_test.txt", col.names="label")
+test.subjects <- read.table("test/subject_test.txt", col.names="subject")
+test.data <- read.table("test/X_test.txt")
+train.labels <- read.table("train/y_train.txt", col.names="label")
+train.subjects <- read.table("train/subject_train.txt", col.names="subject")
+train.data <- read.table("train/X_train.txt")
 
-# 1. Merges the training and the test sets to create one data set.
+# put it together in the format of: subjects, labels, everything else
+data <- rbind(cbind(test.subjects, test.labels, test.data),
+              cbind(train.subjects, train.labels, train.data))
 
-tmp1 <- read.table("train/X_train.txt")
-tmp2 <- read.table("test/X_test.txt")
-X <- rbind(tmp1, tmp2)
+## step 2
+# read the features
+features <- read.table("features.txt", strip.white=TRUE, stringsAsFactors=FALSE)
+# only retain features of mean and standard deviation
+features.mean.std <- features[grep("mean\\(\\)|std\\(\\)", features$V2), ]
 
-tmp1 <- read.table("train/subject_train.txt")
-tmp2 <- read.table("test/subject_test.txt")
-S <- rbind(tmp1, tmp2)
+# select only the means and standard deviations from data
+# increment by 2 because data has subjects and labels in the beginning
+data.mean.std <- data[, c(1, 2, features.mean.std$V1+2)]
 
-tmp1 <- read.table("train/y_train.txt")
-tmp2 <- read.table("test/y_test.txt")
-Y <- rbind(tmp1, tmp2)
+## step 3
+# read the labels (activities)
+labels <- read.table("activity_labels.txt", stringsAsFactors=FALSE)
+# replace labels in data with label names
+data.mean.std$label <- labels[data.mean.std$label, 2]
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## step 4
+# first make a list of the current column names and feature names
+good.colnames <- c("subject", "label", features.mean.std$V2)
+# then tidy that list
+# by removing every non-alphabetic character and converting to lowercase
+good.colnames <- tolower(gsub("[^[:alpha:]]", "", good.colnames))
+# then use the list as column names for data
+colnames(data.mean.std) <- good.colnames
 
-features <- read.table("features.txt")
-indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, indices_of_good_features]
-names(X) <- features[indices_of_good_features, 2]
-names(X) <- gsub("\\(|\\)", "", names(X))
-names(X) <- tolower(names(X))
+## step 5
+# find the mean for each combination of subject and label
+aggr.data <- aggregate(data.mean.std[, 3:ncol(data.mean.std)],
+                       by=list(subject = data.mean.std$subject, 
+                               label = data.mean.std$label),
+                       mean)
 
-# 3. Uses descriptive activity names to name the activities in the data set.
-
-activities <- read.table("activity_labels.txt")
-activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
-Y[,1] = activities[Y[,1], 2]
-names(Y) <- "activity"
-
-# 4. Appropriately labels the data set with descriptive activity names.
-
-names(S) <- "subject"
-cleaned <- cbind(S, Y, X)
-write.table(cleaned, "merged_clean_data.txt")
-
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
-
-uniqueSubjects = unique(S)[,1]
-numSubjects = length(unique(S)[,1])
-numActivities = length(activities[,1])
-numCols = dim(cleaned)[2]
-result = cleaned[1:(numSubjects*numActivities), ]
-
-row = 1
-for (s in 1:numSubjects) {
-  for (a in 1:numActivities) {
-    result[row, 1] = uniqueSubjects[s]
-    result[row, 2] = activities[a, 2]
-    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
-    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
-    row = row+1
-  }
-}
-write.table(result, "data_set_with_the_averages.txt")
+## step nothing
+# write the data for course upload
+write.table(format(aggr.data, scientific=T), "data_set_with_the_averages.txt",
+            row.names=F, col.names=F, quote=2)
